26c16691e582c5fd5751d0f2c3b2509f94476e80
chientranthien <vinadonquixote@gmail.com>
2023-12-08 10:46:46 +0800

    Improve build script to move binary to folder

--- be/script/build.sh
+++ be/script/build.sh
@@ -4,25 +4,25 @@
 
 build_user_service() {
  echo_info "Building user_service"
- go build -o bin/user_service ./internal/service/user/main/...
+ go build -o bin/user_service/user_service ./internal/service/user/main/...
  echo_finish
 }
 
 build_wallet_service() {
  echo_info "Building wallet_service"
- go build -o bin/wallet_service ./internal/service/wallet/main/...
+ go build -o bin/wallet_service/wallet_service ./internal/service/wallet/main/...
  echo_finish
 }
 
 build_http_gateway() {
  echo_info "Building http_gateway"
- go build -o bin/http_gateway ./internal/service/http/main/...
+ go build -o bin/http_gateway/http_gateway ./internal/service/http/main/...
  echo_finish
 }
 
 build_event_handler() {
  echo_info "Building event_handler"
- go build -o bin/event_handler ./internal/service/event_handler/main/...
+ go build -o bin/event_handler/event_handler ./internal/service/event_handler/main/...
  echo_finish
 }
 
