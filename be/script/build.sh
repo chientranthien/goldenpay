96371192b45bb3ad760652012c23d98c87548956
Chien Tran <vinadonquixote@gmail.com>
2023-12-18 14:53:15 +0800

    Improve the build script to be able to build for any service

--- be/script/build.sh
+++ be/script/build.sh
@@ -2,39 +2,78 @@
 
 . "$(git rev-parse --show-toplevel || echo ".")/be/script/common.sh"
 
-build_user_service() {
- echo_info "Building user_service"
- go build -o bin/user_service/user_service ./internal/service/user/main/...
- cp -n ./internal/service/user/config/config.yaml bin/user_service/
- echo_finish
+usage() {
+  cat <<EOF
+Build binary artifacts this service. Build target could be any folder under the
+'./internal/service' one, such as:
+
+  ./script/build.sh service
+
+Or, if you trigger this script via make:
+
+  make build/$service
+
+You can also build all packages under './internal/service' into binaries by a special target:
+'all'.
+
+  make build/all
+
+To do cross compilation, such as build binary for Linux while working on Mac
+
+  GOOS=linux make build/all
+
+See "make gen" if you are looking for code generation. Note that some build
+recipes might fail if the required generated code is missing.
+EOF
 }
 
-build_wallet_service() {
- echo_info "Building wallet_service"
- go build -o bin/wallet_service/wallet_service ./internal/service/wallet/main/...
- cp -n  ./internal/service/wallet/config/config.yaml bin/wallet_service/
- echo_finish
-}
-
-build_http_gateway() {
- echo_info "Building http_gateway"
- go build -o bin/http_gateway/http_gateway ./internal/service/http/main/...
- cp -n  ./internal/service/http/config/config.yaml bin/http_gateway/
- echo_finish
-}
-
-build_event_handler() {
- echo_info "Building event_handler"
- go build -o bin/event_handler/event_handler ./internal/service/event_handler/main/...
- cp -n  ./internal/service/event_handler/config/config.yaml bin/event_handler/
- echo_finish
-}
 
 build() {
-  build_user_service
-  build_wallet_service
-  build_http_gateway
-  build_event_handler
+  service=$1
+  echo_info "Building $service"
+  out="./bin/$service/exc"
+  target="./internal/service/$service/main/..."
+  go build -ldflags="$GO_LDFLAGS"  -v -o $out $target
+  ls -lah -d $out
 }
 
-build
+check_target() {
+  service="$1"
+  if [ -z $1 ]; then
+    usage
+    exit
+  fi
+
+  target="./internal/service/$service/main"
+  if [ -d "$target" ]; then
+    build "$service"
+  else
+    echo_warn "$service is not exists! Here's how to use build script"
+    usage
+  fi
+}
+
+build_all() {
+  # Build
+  for service in $(ls ./internal/service/ -1) ; do
+    # skip the folder if there's no go file in it
+    ls "./internal/service/$service/main/"*.go >/dev/null 2>&1 || (echo_warn "skipped $service" && continue)
+    # build the service
+    build "$service"
+  done
+}
+
+case "$1" in
+all)
+  build_all
+  exit ;;
+
+-h | --help)
+  usage
+  exit
+  ;;
+*)
+  check_target "$@"
+  exit
+  ;;
+esac
